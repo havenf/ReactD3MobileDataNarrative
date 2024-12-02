5efd713bb933e2d1c3faac631d8e7825284b4642
havenf <havenfricke@u.boisestate.edu>
2024-12-01 22:10:56 -0700

    Deploy asset and configuration removal

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  //Remove the line below if debugging
-  base: '/ReactD3MobileDataNarrative/',
 })
